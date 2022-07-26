de048e56d56f9fb7a57c64b22efec547298a308d
Elvin <elveong@gmail.com>
2022-07-26 23:14:11 +0800

    Commit xTesting

---
+++ .Net Core/TestCMSCoreAPI/TestUnit/Controller/CustomerControllerTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ServiceManager.DTO;
+using ServiceManager.Interface;
+using TestCMSCoreAPI.Controllers;
+using TestUnit.Service;
+
+namespace TestUnit.Controller
+{
+    public class CustomerTest
+    {
+        private readonly CustomerServiceMock _service;
+
+        public CustomerTest()
+        {
+            _service = new CustomerServiceMock();
+        }
+
+        [Fact]
+        public async Task Get_WhenCalled_ReturnsOkResult()
+        {
+            var service = new Mock<ICustomerService>();
+            service.Setup(_ => _.GetCustomerList()).ReturnsAsync(_service.GetCustomerList());
+            var controller = new CustomerController(service.Object);
+
+            var result = await controller.GetAllCustomers() as OkObjectResult;
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Get_WhenCalled_ReturnsAllCustomers()
+        {
+            var service = new Mock<ICustomerService>();
+            service.Setup(_ => _.GetCustomerList()).ReturnsAsync(_service.GetCustomerList());
+            var controller = new CustomerController(service.Object);
+
+            var result = (OkObjectResult) await controller.GetAllCustomers();
+            var items = Assert.IsType<List<CustomerDTO.FullDetail>>(result.Value);
+            Assert.Equal(3, items.Count);
+        }
+
+        [Fact]
+        public async Task GetById_UnknownGuidPassed_ReturnsNotFoundResult()
+        {
+            var guid = Guid.NewGuid();
+
+            var service = new Mock<ICustomerService>();
+            service.Setup(_ => _.GetCustomerByID(guid)).ReturnsAsync(_service.GetByID(guid));
+            var controller = new CustomerController(service.Object);
+
+            var result = await controller.GetCustomerByID(guid);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetById_ContainedGuid_ReturnsOKFoundResult()
+        {
+            var guid = new Guid("78f95907-3b13-4b87-82a4-af5c702111ae");
+
+            var service = new Mock<ICustomerService>();
+            service.Setup(_ => _.GetCustomerByID(guid)).ReturnsAsync(_service.GetByID(guid));
+            var controller = new CustomerController(service.Object);
+
+            var result = (OkObjectResult) await controller.GetCustomerByID(guid);
+            var customer = Assert.IsType<CustomerDTO.FullDetail>(result.Value);
+
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("A001", customer.Detail.CustomerID);
+        }
+
+    }
+}
---
+++ .Net Core/TestCMSCoreAPI/TestUnit/Service/CustomerServiceMock.cs
@@ -0,0 +1,59 @@
+﻿using ServiceManager.DTO;
+
+namespace TestUnit.Service
+{
+    internal class CustomerServiceMock
+    {
+        private readonly List<CustomerDTO.FullDetail> customers;
+
+        public CustomerServiceMock()
+        {
+            customers = new List<CustomerDTO.FullDetail>()
+            {
+                new CustomerDTO.FullDetail() 
+                {
+                    ID = new Guid("78f95907-3b13-4b87-82a4-af5c702111ae"),
+                    Detail = new CustomerDTO.Detail()
+                    {
+                        CustomerID = "A001",
+                        FullName = "Benjamin",
+                        IDType = "NRIC",
+                        IDNo = "8003"
+                    }
+                },
+                new CustomerDTO.FullDetail()
+                {
+                    ID = new Guid("a2ab1dc8-2d10-4c3f-a413-43c5f5208041"),
+                    Detail = new CustomerDTO.Detail()
+                    {
+                        CustomerID = "A002",
+                        FullName = "Elvin",
+                        IDType = "NRIC",
+                        IDNo = "8002"
+                    }
+                },
+                new CustomerDTO.FullDetail()
+                {
+                    ID = new Guid("b1421b0b-8942-4693-895b-0573b77cd2fc"),
+                    Detail = new CustomerDTO.Detail()
+                    {
+                        CustomerID = "A001",
+                        FullName = "Allan",
+                        IDType = "NRIC",
+                        IDNo = "8001"
+                    }
+                },
+            };
+        }
+
+        public List<CustomerDTO.FullDetail> GetCustomerList()
+        {
+            return customers;
+        }
+
+        public CustomerDTO.FullDetail GetByID(Guid ID)
+        {
+            return customers.Where(c => c.ID == ID).FirstOrDefault();
+        }
+    }
+}
