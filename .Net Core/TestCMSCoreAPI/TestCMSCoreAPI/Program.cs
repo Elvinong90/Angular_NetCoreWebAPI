15ad0e3ced5d75c15b36fc98f66d2be4ff4ab193
Elvin <elveong@gmail.com>
2022-08-18 23:04:34 +0800

    remove Azure Profile

--- .Net Core/TestCMSCoreAPI/TestCMSCoreAPI/Program.cs
+++ .Net Core/TestCMSCoreAPI/TestCMSCoreAPI/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(CustomCorPolicy, 
         builder => builder.AllowAnyHeader()
         .AllowAnyMethod()
-        .WithOrigins("http://localhost:4200", "https://testcmsangular.azurewebsites.net"));
+        .WithOrigins("http://localhost:4200", "http://localhost:8000"));
 });
 
 builder.Services.AddEndpointsApiExplorer();
